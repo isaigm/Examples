3d4aa699e8e02616f7c8ef8e0149b38b479c2a6e
IsaíGM <50834416+J-esimo@users.noreply.github.com>
2019-08-26 15:08:45 -0500

    Update ArbolesBinarios.cpp

--- ArbolesBinarios.cpp
+++ ArbolesBinarios.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <ncurses.h>
 class Nodo{
 public:
     friend class Arbol;
@@ -20,14 +20,20 @@ public:
         }
     }
     Arbol(){}
+    void eliminarDato(int dato){
+        eliminarDato(&raiz, dato);
+    }
     void insertar(int dato){
         insertar(&raiz, dato);
     }
-    void inorden(){
-        inorden(raiz);
+    void inorden(int x, int y){
+        inorden(raiz, x, y);
     }
     int altura(){
         return altura(raiz);
+    }
+    int nodos(){
+        return nodos(raiz);
     }
 private:
     Nodo *raiz = nullptr;
@@ -39,11 +45,12 @@ private:
             else if((*raiz_)->dato < dato) insertar(&(*raiz_)->der, dato);
         }
     }
-    void inorden(Nodo *raiz){
+    void inorden(Nodo *raiz, int x, int y){
         if(raiz != nullptr){
-            inorden(raiz->izq);
-            std::cout << raiz->dato << std::endl;
-            inorden(raiz->der);
+            inorden(raiz->izq, x - 3, y + 1);
+            mvprintw(y, x, "%d", raiz->dato);
+            refresh();
+            inorden(raiz->der, x + 3, y + 1);
         }
     }
     int altura(Nodo *raiz_){
@@ -56,12 +63,50 @@ private:
 
         }
     }
+    int nodos(Nodo * raiz_){
+        if(raiz_ == nullptr){
+            return 0;
+        }else{
+            int der = nodos(raiz_->der);
+            int izq = nodos(raiz_->izq);
+            return 1 + izq + der;
+        }
+    }
+    void eliminarDato(Nodo **raiz_, int dato){
+        if(*raiz_ != nullptr){
+            if((*raiz_)->dato == dato){
+                if((*raiz_)->izq == nullptr && (*raiz_)->der == nullptr) eliminarNodo(*raiz_);
+                else{
+                    Nodo *aux = *raiz_;
+                    if((*raiz_)->izq != nullptr && (*raiz_)->der == nullptr){
+                        *raiz_ = (*raiz_)->izq;
+                        eliminarNodo(aux);
+                    }else if((*raiz_)->der != nullptr && (*raiz_)->izq == nullptr){
+                        *raiz_ = (*raiz_)->der;
+                        eliminarNodo(aux);
+                    }
+                }
+            }else if(dato < (*raiz_)->dato) eliminarDato(&(*raiz_)->izq, dato);
+            else if(dato > (*raiz_)->dato) eliminarDato(&(*raiz_)->der, dato);
+        }
+    }
+    void eliminarNodo(Nodo *nodo){
+        delete nodo;
+        nodo = nullptr;
+    }
 };
 int main(){
-    Arbol arbol {10, 11, -10, -12, -13, 12, -15};
-    arbol.inorden();
-    int h = arbol.altura();
-    std::cout << h << std::endl;
+    int rows, cols;
+    initscr();
+    keypad(stdscr, TRUE);
+    curs_set(FALSE);
+    noecho();
+    getmaxyx(stdscr, rows, cols);
+    Arbol arbol {10, 11, -10, -12, -13, 12, -15, -11, -9, -8, 13, 20};
+    arbol.eliminarDato(13);
+    arbol.eliminarDato(12);
+    arbol.inorden(cols/2 - 1,  rows/2 - 10);
+    addch('\n');
+    refresh();
     return 0;
 }
-
