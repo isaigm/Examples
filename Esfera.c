6ca9f395216d00a5b3eafaa35bec149816178999
IsaíGM <50834416+J-esimo@users.noreply.github.com>
2019-09-20 14:25:30 +0000

    Update Esfera.c

--- Esfera.c
+++ Esfera.c
@@ -4,16 +4,28 @@
 void display(void);
 void keys(unsigned char key, int x, int y);
 void reshape(int x, int y);
-GLfloat posx = 0, posy = 0;
+typedef struct Sphere{
+  GLfloat posx, posy;
+  GLfloat directionx;
+  GLfloat directiony;
+  GLfloat radius;
+}Sphere;
+Sphere sphere;
 int main(int argc, char **argv)
 {
+    sphere.posx = 0;
+    sphere.posy = 0;
+    sphere.radius = 1;
+    sphere.directionx = 1;
+    sphere.directiony = 1;
     glutInit(&argc, argv);
     glutInitWindowSize(1280, 720);
     glutInitWindowPosition(10, 10);
-    glutCreateWindow("Hola, mundo!");
+    glutCreateWindow("Esfera!");
     glutDisplayFunc(display);
     glutKeyboardFunc(keys);
     glutReshapeFunc(reshape);
+
     glutMainLoop();
     return 0;
 }
@@ -21,28 +33,35 @@ void display(){
     glMatrixMode(GL_MODELVIEW);
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-    glTranslatef(posx - 3, posy, -7.0);
+    glTranslatef(sphere.posx - 3, sphere.posy, -7.0);
     glColor3f((GLfloat)0.9, (GLfloat)0.3, (GLfloat)0.2);
     glScalef(1.0, 1.0, 1.0);
-    glutSolidSphere(1, 100, 100);
+    glutSolidSphere(sphere.radius, 100, 100);
+    sphere.posx += sphere.directionx * 0.006f;
+    sphere.posy += sphere.directiony * 0.006f;
+    if(sphere.posx > 6.5) sphere.directionx = -1;
+    else if(sphere.posx < -1) sphere.directionx = 1;
+    if(sphere.posy > 1.9) sphere.directiony = -1;
+    else if(sphere.posy < -2) sphere.directiony = 1;
+    glutPostRedisplay();
     glFlush();
 }
 void keys(unsigned char key, int x, int y){
     switch (key) {
     case 'd':
-        posx += 0.1f;
+        sphere.posx += 0.1f;
         glutPostRedisplay();
         break;
     case 'a':
-        posx -= 0.1f;
+        sphere.posx -= 0.1f;
         glutPostRedisplay();
         break;
     case 'w':
-        posy += 0.1f;
+        sphere.posy += 0.1f;
         glutPostRedisplay();
         break;
     case 's':
-        posy -= 0.1f;
+        sphere.posy -= 0.1f;
         glutPostRedisplay();
         break;
     case 'x':
@@ -59,3 +78,4 @@ void reshape(int x, int y)
     glMatrixMode(GL_MODELVIEW);
     glViewport(0, 0, x, y);
 }
+
