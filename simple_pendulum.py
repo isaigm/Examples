3c06aeb445031ae7a02a4e76e4b67121ff4ff399
Isaí <50834416+isaigm@users.noreply.github.com>
2020-10-02 19:46:54 -0500

    Update simple_pendulum.py

--- simple_pendulum.py
+++ simple_pendulum.py
@@ -1,23 +1,28 @@
-import numpy as np, math, pygame as py
+import math, os, pygame as py
 from pygame.locals import *
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 py.init()
 py.mixer.quit()
 g = 9.8
-l = 150
+l = 250
 omega = 0
-theta = math.radians(180)
+theta = math.radians(90)
 py.display.set_caption("Simple pendulum simulation")
 window = py.display.set_mode([800, 600])
 clock = py.time.Clock()
 dt = clock.tick(60) / 100
+air_resistance = 0.1
+mass = 10
 base_x = 400
 base_y = 300
+#Euler's method
 while True:
     for e in py.event.get():
         if e.type == QUIT:
             py.quit()
             break
-    omega = omega - (g / l) * dt * math.sin(theta)
+    omega = omega - ((air_resistance / mass) * omega + (g / l) * math.sin(theta)) * dt
     theta = theta + dt * omega
     x = round(l * math.sin(theta)) 
     y = round(l * math.cos(theta))
