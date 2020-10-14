a2760923a55f6e31dcf61f814145f9e7be822eb9
Isaí <50834416+isaigm@users.noreply.github.com>
2020-10-14 10:41:25 -0500

    Create fractal_tree.py

---
+++ fractal_tree.py
@@ -0,0 +1,43 @@
+import os
+import pygame as py
+
+from pygame.locals import *
+
+
+def get_vector(p, q) -> py.math.Vector2:
+    return p - q
+
+
+def draw_tree(surface: py.Surface, s: py.math.Vector2, en: py.math.Vector2, depth):
+    if depth >= 1:
+        py.draw.line(surface, (0, 0, 0), (s.x, 600 - s.y), (en.x, 600 - en.y))
+        v = get_vector(en, s)
+        left = v.rotate(30).normalize() * (v.length() - v.length() * 0.34)
+        right = v.rotate(-30).normalize() * (v.length() - v.length() * 0.34)
+        py.draw.line(surface, (0, 0, 0), (en.x, 600 - en.y), (en.x + left.x, 600 - (left.y + en.y)))
+        py.draw.line(surface, (0, 0, 0), (en.x, 600 - en.y), (en.x + right.x, 600 - (right.y + en.y)))
+        draw_tree(surface, en, py.Vector2(en.x + left.x, left.y + en.y), depth - 1)
+        draw_tree(surface, en, py.Vector2(en.x + right.x, right.y + en.y), depth - 1)
+
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+py.init()
+py.mixer.quit()
+
+window = py.display.set_mode([800, 600])
+clock = py.time.Clock()
+end = py.math.Vector2(400, 200)
+start = py.math.Vector2(400, 10)
+canvas = py.Surface((800, 600))
+canvas.fill((255, 255, 255))
+draw_tree(canvas, start, end, 10)
+while True:
+    for e in py.event.get():
+        if e.type == QUIT:
+            py.quit()
+            break
+
+    window.fill((255, 255, 255))
+    window.blit(canvas, (0, 0))
+    py.display.flip()
+    clock.tick(60)
