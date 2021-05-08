b0cf765b956b611b339a78ec35199b2f72daafb4
Isaí <50834416+isaigm@users.noreply.github.com>
2021-05-08 13:02:45 -0500

    Update random_walk.py

--- random_walk.py
+++ random_walk.py
@@ -2,14 +2,14 @@ import random
 import pygame as py
 import sys
 from pygame.locals import *
-
+#terrain generator?
 WIDTH, HEIGHT = 800, 600
 py.init()
 
 py.display.set_caption("Random Walk")
 window = py.display.set_mode([WIDTH, HEIGHT])
 canvas = py.Surface([WIDTH, HEIGHT])
-canvas.fill((255, 255, 255))
+canvas.fill((0, 0, 255))
 RECT_SIZE = 10
 clock = py.time.Clock()
 x, y = round(WIDTH / 2), round(HEIGHT / 2)
@@ -18,8 +18,8 @@ while True:
         if e.type == QUIT:
             py.quit()
             sys.exit()
-    window.fill((255, 255, 255))
-    py.draw.rect(canvas, (0, 0, 0), (x, y, RECT_SIZE, RECT_SIZE), 2)
+    window.fill((0, 0, 255))
+    py.draw.rect(canvas, (155,118,83), (x, y, RECT_SIZE, RECT_SIZE))
     window.blit(canvas, (0, 0))
     py.display.flip()
     n = random.randint(0, 4)
