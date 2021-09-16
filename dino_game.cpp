a6ee8467458a1a696565b5b69d1dd3e520c5fdca
Isaí <50834416+isaigm@users.noreply.github.com>
2021-09-15 21:35:53 -0500

    Update dino_game.cpp
    i hate gravity

--- dino_game.cpp
+++ dino_game.cpp
@@ -3,15 +3,15 @@
 const static char* SPRISHEET_FILE = "C:\\Users\\isaig\\Desktop\\sprites.png";
 const static int WIDTH = 600;
 const static int HEIGHT = 320;
-const static int dino_coords[][4] = { {846, 0, 43, 48},
+const static float gravity = 900.0f;
+const static int dino_frames[][4] = { {846, 0, 43, 48},
 	{937, 0, 43, 48}, 
 	{980, 0, 43, 48} };
 
 
 sf::IntRect coords_to_rect_int(int idx)
 {
-	sf::IntRect rect(dino_coords[idx][0], dino_coords[idx][1], dino_coords[idx][2], dino_coords[idx][3]);
-
+	sf::IntRect rect(dino_frames[idx][0], dino_frames[idx][1], dino_frames[idx][2], dino_frames[idx][3]);
 	return rect;
 }
 class player
@@ -22,7 +22,8 @@ public:
 		sp.setTexture(texture);
 		sp.setTextureRect(coords_to_rect_int(curr_frame_anim));
 		x = 20;
-		y = 125;
+		ground = y = 125;
+		y_speed = -500;
 		sp.setPosition(x, y);
 	}
 	void animate()
@@ -35,14 +36,27 @@ public:
 	}
 	void update(float dt)
 	{
-		x += speed * dt;
+		x += x_speed * dt;
+		if (!is_on_ground)
+		{
+			y += y_speed * dt;
+			y_speed += gravity * dt;
+			if (y >= ground)
+			{
+				y = ground;
+				is_on_ground = true;
+				y_speed = -500;
+			}
+	
+		}
 		sp.setPosition({ x, y });
 	}
 	
+	bool is_on_ground = true;
 	sf::Sprite sp;
 	int curr_frame_anim = 0;
-	float x,  y;
-	int speed = 100;
+	float x,  y, ground, y_speed;
+	int x_speed = 100;
 };
 
 
@@ -72,7 +86,6 @@ int main()
 
 	float dt_anim = 0;
 	float dt = 0;
-
 	bool main_bg = true;
 	int factor = 1;
 	sf::Clock clock;
@@ -89,15 +102,20 @@ int main()
 				window.close();
 				break;
 			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Space)
+				{
 				
+					dino.is_on_ground = false;
+				}
 				break;
 			default:
 				break;
 			}
 		}
-		temp_x += dt * dino.speed;
+		
+		temp_x += dt * dino.x_speed;
 		dino.update(dt);
-		view.move(dt * dino.speed, 0);
+		view.move(dt * dino.x_speed, 0);
 		
 
 		if (temp_x >= 1233 - WIDTH) //esto se hace para que antes de recorrer el fondo por completo, se cargue otro en la posicion correcta y asi
@@ -132,4 +150,3 @@ int main()
 
 	return 0;
 }
-
