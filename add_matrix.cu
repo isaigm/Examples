9467d72abcb869173e8dc401d1e75424ca165d4f
Isaí García Mendoza <50834416+isaigm@users.noreply.github.com>
2021-11-12 13:28:25 -0600

    Update add_matrix.cu

--- add_matrix.cu
+++ add_matrix.cu
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <assert.h>
-#define N 10000
+#define N 1000
 #define MAT_GPU 1 << 2
 #define MAT_CPU 1 << 3
 
@@ -18,7 +18,7 @@ __global__ void add(int *a, int *b, int *c, int rows, int cols)
 {
     //----------                                      dimension
     int x = blockIdx.x * blockDim.x + threadIdx.x; //[0 0 0 0 0] [0 0 0 0 0] <- bloque
-    int y = blockIdx.y * blockDim.y + threadIdx.y; // |_ threadIdx.x
+    int y = blockIdx.y * blockDim.y + threadIdx.y; // |-> threadIdx.x
     if (x < cols && y < rows)
     {
         int a_e = get_element(a, cols, x, y);
@@ -26,7 +26,20 @@ __global__ void add(int *a, int *b, int *c, int rows, int cols)
         set_element(c, a_e + b_e, cols, x, y);
     }
 }
-
+__global__ void mult(int *a, int *b, int *c, int n)
+{
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n && j < n)
+    {
+        int res = 0;
+        for (int k = 0; k < n; k++)
+        {
+            res += a[k + i * n] * b[j + k * n];
+        }
+        c[j + i * n] = res;
+    }
+}
 int *allocate_matrix(int kind, int rows, int cols)
 {
     int *matrix = NULL;
@@ -44,11 +57,42 @@ void random_fill_matrix(int *matrix, int rows, int cols)
 {
     for (int idx = 0; idx < rows * cols; idx++)
     {
-        matrix[idx] = rand() % (1 << 8);
+        matrix[idx] = rand() % 10;
     }
+}
+void cpu_mul(int *a, int *b, int *c, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int res = 0;
+
+            for (int k = 0; k < n; k++)
+            {
+                res += a[k + i * n] * b[j + k * n];
+            }
+            c[j + i * n] = res;
+        }
+    }
+}
+void print_matrix(int *m, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+
+    {
+        for (int j = 0; j < cols; j++)
+
+        {
+            printf("%d ", m[j + i * cols]);
+        }
+        printf("\n");
+    }
+    printf("-----------------------------------\n");
 }
 int main()
 {
+    srand(time(nullptr));
     int *a_host_matrix = allocate_matrix(MAT_CPU, N, N);
     int *b_host_matrix = allocate_matrix(MAT_CPU, N, N);
     int *result_host_matrix = allocate_matrix(MAT_CPU, N, N);
@@ -59,23 +103,20 @@ int main()
 
     random_fill_matrix(a_host_matrix, N, N);
     random_fill_matrix(b_host_matrix, N, N);
+    //print_matrix(a_host_matrix, N, N);
+    //print_matrix(b_host_matrix, N, N);
+    //cpu_mul(a_host_matrix, b_host_matrix, result_host_matrix, N);
+    //print_matrix(result_host_matrix, N, N);
     cudaMemcpy((void *)a_device_matrix, (void *)a_host_matrix, sizeof(int) * N * N, cudaMemcpyHostToDevice);
     cudaMemcpy((void *)b_device_matrix, (void *)b_host_matrix, sizeof(int) * N * N, cudaMemcpyHostToDevice);
     dim3 bdim(int(ceil(N / 32.0f)), int(ceil(N / 32.0f))); //la estructura de los hilos es en forma de grid, donde cada celda es una matriz de 32x32
     dim3 tdim(32, 32);
     clock_t start = clock();
-    add<<<bdim, tdim>>>(a_device_matrix, b_device_matrix, result_device_matrix, N, N);
+    mult<<<bdim, tdim>>>(a_device_matrix, b_device_matrix, result_device_matrix, N);
     cudaDeviceSynchronize();
     cudaMemcpy((void *)result_host_matrix, (void *)result_device_matrix, sizeof(int) * N * N, cudaMemcpyDeviceToHost);
     clock_t end = clock();
     printf("tiempo de ejecucion: %f\n", double(end - start) / CLOCKS_PER_SEC);
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            assert(result_host_matrix[j + i * N] == a_host_matrix[j + i * N] + b_host_matrix[j + i * N]);
-        }
-    }
-
+    //print_matrix(result_host_matrix, N, N);
     return 0;
 }
