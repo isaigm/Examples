231ed172c2c7f31bd02a31e38ad70b246287dce3
Isaí García Mendoza <50834416+isaigm@users.noreply.github.com>
2022-02-18 19:57:07 -0600

    usando sf::RenderTextur

--- alfombra_sierpinski.cpp
+++ alfombra_sierpinski.cpp
@@ -1,41 +1,45 @@
 #include <SFML/Graphics.hpp>
-#include <vector>
 #include <iostream>
 sf::RectangleShape crear_cuadro(float x, float y, float lado)
 {
     sf::RectangleShape cuadro;
 
     cuadro.setSize({lado, lado});
-    cuadro.setPosition(x - lado / 2, y - lado/2);
+    cuadro.setPosition(x - lado / 2, y - lado / 2);
     cuadro.setFillColor(sf::Color::Black);
     return cuadro;
 }
-void sierp(std::vector<sf::RectangleShape> &cuadros, float lado_real, float lado, float x, float y, int n)
+void sierp(sf::RenderTexture &rt, float lado_real, float lado, float x, float y, int n)
 {
     if (n == 0)
         return;
-    cuadros.push_back(crear_cuadro(x * 699, y * 699, lado_real));
+    rt.draw(crear_cuadro(x * 600, y * 600, lado_real));
 
-    sierp(cuadros, lado_real / 3.0f, lado / 3.0f, x / 3.0f, y / 3.0f, n - 1);
-    sierp(cuadros, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 1.0f / 3.0f, y / 3.0f, n - 1);
-    sierp(cuadros, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 2.0f / 3.0f, y / 3.0f, n - 1);
-    sierp(cuadros, lado_real / 3.0f, lado / 3.0f, x / 3.0f, y / 3.0f + 1 / 3.0f, n - 1);
-    sierp(cuadros, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 2 / 3.0f, y / 3.0f + 1 / 3.0f, n - 1);
-    sierp(cuadros, lado_real / 3.0f, lado, x / 3.0f, y / 3.0f + 2 / 3.0f, n - 1);
-    sierp(cuadros, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 1 / 3.0f, y / 3.0f + 2 / 3.0f, n - 1);
-    sierp(cuadros, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 2 / 3.0f, y / 3.0f + 2 / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado / 3.0f, x / 3.0f, y / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 1.0f / 3.0f, y / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 2.0f / 3.0f, y / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado / 3.0f, x / 3.0f, y / 3.0f + 1 / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 2 / 3.0f, y / 3.0f + 1 / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado, x / 3.0f, y / 3.0f + 2 / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 1 / 3.0f, y / 3.0f + 2 / 3.0f, n - 1);
+    sierp(rt, lado_real / 3.0f, lado / 3.0f, x / 3.0f + 2 / 3.0f, y / 3.0f + 2 / 3.0f, n - 1);
 }
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(699, 699), "Alfombra de sierpinski");
-    std::vector<sf::RectangleShape> cuadros;
+    sf::RenderWindow window(sf::VideoMode(600, 600), "Alfombra de sierpinski");
     window.setVerticalSyncEnabled(true);
-
-    sierp(cuadros, 699/3, 1, 0.5f, 0.5f,5);
+    sf::Sprite sprite;
+    sf::RenderTexture rt;
+    rt.create(600, 600);
+    
+    rt.clear(sf::Color::White);
+    sierp(rt, 600 / 3, 1, 0.5f, 0.5f, 5);
+    rt.display();
+    sprite.setTexture(rt.getTexture());
     while (window.isOpen())
     {
-     
+
         sf::Event ev;
         while (window.pollEvent(ev))
         {
@@ -47,10 +51,7 @@ int main()
         }
 
         window.clear(sf::Color::White);
-        for (auto &cuadro : cuadros)
-        {
-            window.draw(cuadro);
-        }
+        window.draw(sprite);
         window.display();
     }
 
