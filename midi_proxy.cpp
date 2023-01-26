d9b9f5b636aece749c3744f5e3dd9228edfede71
Isaí García Mendoza <50834416+isaigm@users.noreply.github.com>
2023-01-25 18:14:40 -0600

    Update midi_proxy.cpp

--- midi_proxy.cpp
+++ midi_proxy.cpp
@@ -10,19 +10,26 @@ void CALLBACK MidiInProc(
     DWORD_PTR dwParam1,
     DWORD_PTR dwParam2)
 {
-    dwParam1 &= 0xFF00FFFF;
-    dwParam1 |= (0x7F0000);
+    uint8_t firstByte = dwParam1 & 0xFF;
+    if (firstByte == 0x90)
+    {
+        dwParam1 &= 0xFF00FFFF;
+        dwParam1 |= (0x7F0000);
+    }
     midiOutShortMsg(proxy, dwParam1);
 }
 int main()
 {
     HMIDIIN instrument = 0;
+    //id between [0, midiOutGetNumDevs() - 1]
+                            //<- sometimes this number changes
     if (midiOutOpen(&proxy, 2, NULL, 0, NULL) == MMSYSERR_NOERROR)
     {
-        std::cout << "ok\n";
+        std::cout << "ok\n";//use loop midi to create a virtual midi device, in the DAW we going to use this virtual 
+                            //midi instead of the physical one, which is not working properly (notes are quiet in random moments)
     }
-    std::cout << "number of midi devices: " << midiInGetNumDevs() << "\n";
-
+    std::cout << "number of midi devices: " << midiInGetNumDevs() << "\n"; //id between [0, midiInGetNumDevs() - 1]
+                                //<- sometimes this number changes
     if (midiInOpen(&instrument, 0, reinterpret_cast<DWORD_PTR>(MidiInProc), 0, CALLBACK_FUNCTION) == MMSYSERR_NOERROR)
     {
         std::cout << "midi device opened\n";
