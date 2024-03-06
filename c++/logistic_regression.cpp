eed54ab0c0ff2f199535b936ba78b87034cc1e0e
Isaí García <50834416+isaigm@users.noreply.github.com>
2024-03-06 12:41:35 -0600

    Update logistic_regression.cpp

--- c++/logistic_regression.cpp
+++ c++/logistic_regression.cpp
@@ -46,7 +46,9 @@ namespace utils
         std::ifstream file(path);
         bool seenFirstLine = false;
         if (!file.is_open())
-            return;
+        {
+            throw std::runtime_error("file doesnt exist");
+        }
 
         while (std::getline(file, line))
         {
@@ -67,43 +69,47 @@ namespace utils
 };
 
 template <class T>
-class LinearRegressor
+class LogisticRegressor
 {
 public:
-    LinearRegressor(size_t inputs) : m_coefficients(inputs + 1), m_inputs(inputs) {}
+    LogisticRegressor(size_t inputs) : m_coefficients(inputs + 1), m_inputs(inputs) {}
     const std::vector<T> &getCoefficients()
     {
         return m_coefficients;
     }
     void fit(std::vector<std::vector<T>> &dataSet, T learningRate, size_t batchSize, size_t epochs)
     {
-        auto getRandom = [](float min, float max)
+        auto getRandom = [](double min, double max)
         {
-            float r = float(rand()) / float(RAND_MAX);
+            double r = double(rand()) / double(RAND_MAX);
             return r * (max - min) + min;
         };
         for (size_t i = 0; i < m_coefficients.size(); i++)
         {
-            m_coefficients[i] = getRandom(-0.25f, 0.25f);
+            m_coefficients[i] = getRandom(-1.0, 1.0);
         }
         std::random_device rd;
         std::mt19937 g(rd());
-        size_t init = 0;
         for (size_t i = 0; i < epochs; i++)
         {
-            if (init + batchSize >= dataSet.size())
-            {
-                batchSize = (dataSet.size() - init - 1);
-            }
+            size_t init = 0;
             std::shuffle(dataSet.begin(), dataSet.end(), g);
-            auto start = dataSet.begin() + init;
-            auto end = dataSet.begin() + init + batchSize;
-            std::vector<std::vector<T>> result(batchSize);
-            
-            copy(start, end, result.begin());
-            sgd(result, learningRate);
+            size_t batchSize_ = batchSize;
+            while (batchSize_ > 0 && init < dataSet.size())
+            {
+                if (init + batchSize_ >= dataSet.size())
+                {
+                    batchSize_ = (dataSet.size() - init - 1);
+                }
+                auto start = dataSet.begin() + init;
+                auto end = dataSet.begin() + init + batchSize_;
+                std::vector<std::vector<T>> result(batchSize_);
 
-            init += batchSize;
+                copy(start, end, result.begin());
+                sgd(result, learningRate);
+                
+                init += batchSize_;
+            }
         }
     }
     T predict(std::vector<T> &input)
@@ -136,26 +142,28 @@ public:
 private:
     void sgd(std::vector<std::vector<T>> &dataSet, T learningRate)
     {
+
         for (size_t i = 0; i < m_coefficients.size(); i++)
         {
             T &currCoef = m_coefficients[i];
-
+            T grad{0};
             for (size_t j = 0; j < dataSet.size(); j++)
             {
                 auto row = dataSet[j];
                 T output = row.back();
                 row.pop_back();
                 T prediction = predict(row);
-                T lr = learningRate / T(dataSet.size());
+                T m = 1.0 / T(dataSet.size());
                 if (i == 0)
                 {
-                    currCoef -= lr * (prediction - output);
+                    grad += m * (prediction - output);
                 }
                 else
                 {
-                    currCoef -= lr * (prediction - output) * row[i - 1];
+                    grad += m * (prediction - output) * row[i - 1];
                 }
             }
+            currCoef -= learningRate * grad;
         }
     }
     T sigmoid(T x)
@@ -166,12 +174,12 @@ private:
     size_t m_inputs;
 };
 int main()
-{
-    LinearRegressor<double> lr(15);
+{    
     srand(time(nullptr));
+    LogisticRegressor<double> lr(15);
     std::vector<std::vector<double>> dataSet;
     utils::readFromCSV(dataSet, "data_set.csv");
-    lr.fit(dataSet, 0.15, 150, 70);
+    lr.fit(dataSet, 0.17, 20, 10);
     lr.evaluate(dataSet);
     return 0;
 }
