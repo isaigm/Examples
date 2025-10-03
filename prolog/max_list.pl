e7dafd782a662a8924ad747eb051566649c1013d
Isaí García <50834416+isaigm@users.noreply.github.com>
2025-10-02 18:37:12 -0600

    Update max_list.pl

--- prolog/max_list.pl
+++ prolog/max_list.pl
@@ -11,3 +11,12 @@ maxList([X], X).
 maxList([X | XS], Res) :-
     maxList(XS, Res_),
     max(Res_, X, Res).
+
+
+maxList_([X | Xs], Res) :-
+    maxList_tr(Xs, X, Res).
+
+maxList_tr([], Acc, Acc).
+maxList_tr([X | Xs], Acc, Res) :- 
+    max(X, Acc, Res_),
+    maxList_tr(Xs, Res_, Res).
