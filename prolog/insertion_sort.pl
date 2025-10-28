1a6abd0768f07fd0d8eca99c3198e8064780644b
Isaí García <50834416+isaigm@users.noreply.github.com>
2025-10-28 11:05:35 -0600

    Update and rename max_list.pl to insertion_sort.pl

---
+++ prolog/insertion_sort.pl
@@ -0,0 +1,29 @@
+max(A, B, A) :- A > B.
+
+max(A, B, B) :- A =< B.
+
+maxList([X], X).
+maxList([X | XS], Res) :-
+    maxList(XS, Res_),
+    max(Res_, X, Res).
+
+
+maxList_([X | Xs], Res) :-
+    maxList_tr(Xs, X, Res).
+
+maxList_tr([], Acc, Acc).
+maxList_tr([X | Xs], Acc, Res) :- 
+    max(X, Acc, Res_),
+    maxList_tr(Xs, Res_, Res).
+
+
+insertion_sort(L1, Res) :-
+    insertion_sort_tr(L1, [], Res).
+
+insertion_sort_tr([], Acc, Acc).
+
+insertion_sort_tr(L, Acc, Res) :-
+    maxList(L, E),
+    select(E, L, L_),
+    Acc_ = [E | Acc],
+    insertion_sort_tr(L_, Acc_, Res).
