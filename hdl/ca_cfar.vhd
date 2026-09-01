2881e5f6f0dadecafa1676dd6366dfc982c10afe
Isaí García <50834416+isaigm@users.noreply.github.com>
2026-09-01 17:57:09 -0600

    Increase N_GUARD from 1 to 2 in ca_cfar
    Updated the N_GUARD parameter from 1 to 2 and adjusted related calculations accordingly.

--- hdl/ca_cfar.vhd
+++ hdl/ca_cfar.vhd
@@ -6,16 +6,16 @@ entity ca_cfar is
   generic (
     SAMPLE_W      : integer := 16;    
     N_REF         : integer := 16;   
-    N_GUARD       : integer := 1;    
+    N_GUARD       : integer := 2;    
     ALPHA_W       : integer := 16;    
     ALPHA_FP      : integer := 3188   
 
   );
   port (
     clk, rst : in  std_logic;
-    s_data   : in  std_logic_vector(SAMPLE_W-1 downto 0); 
+    s_data   : in  std_logic_vector(SAMPLE_W - 1 downto 0); 
     s_valid  : in  std_logic;
-    m_cut    : out std_logic_vector(SAMPLE_W-1 downto 0);  
+    m_cut    : out std_logic_vector(SAMPLE_W - 1 downto 0);  
     m_detect : out std_logic;                              
     m_valid  : out std_logic                               
   );
@@ -24,9 +24,9 @@ end entity;
 
 architecture Behavioral of ca_cfar is
   constant TOTAL_SAMPLES : integer := N_REF + 2 * N_GUARD + 1;
-  constant SUM_W : integer := SAMPLE_W + integer(ceil(log2(real(N_REF))));
+  constant SUM_W : integer := SAMPLE_W + integer(ceil(log2(real(TOTAL_SAMPLES))));
   constant T_W: integer := SUM_W + ALPHA_W;
-  
+  constant CUT_IDX: integer := N_REF / 2 + N_GUARD;
   type window_t is array (0 to TOTAL_SAMPLES - 1) of std_logic_vector(SAMPLE_W - 1 downto 0);
   signal window: window_t := (others => (others => '0'));
   signal curr_idx: integer range 0 to TOTAL_SAMPLES := 0;
@@ -37,18 +37,27 @@ architecture Behavioral of ca_cfar is
 begin
   full <= '1' when curr_idx >= TOTAL_SAMPLES else '0';
 
-  ref_sum <= sum_all - resize(unsigned(window(8)), SUM_W) - 
-             resize(unsigned(window(9)), SUM_W) - 
-             resize(unsigned(window(10)), SUM_W);
-  
-  m_cut    <= window(9);
+  m_cut    <= window(CUT_IDX);
   
   m_valid <= '1' when (full = '1' and s_valid = '1') else '0';
   
   threshold <= resize(ref_sum * to_unsigned(ALPHA_FP, T_W), T_W);
   
-  m_detect <= '1' when (shift_left(resize(unsigned(window(9)), T_W), 12) > threshold) else '0';
+  m_detect <= '1' when (shift_left(resize(unsigned(window(CUT_IDX)), T_W), 12) > threshold) else '0';
 
+  process(sum_all, window)
+    variable sum: unsigned(SUM_W - 1 downto 0);
+  begin
+    sum := sum_all;
+    for j in CUT_IDX to CUT_IDX + N_GUARD  loop
+      sum := sum - resize(unsigned(window(j)), SUM_W);
+    end loop;
+    for j in N_REF / 2 to CUT_IDX - 1 loop
+      sum := sum - resize(unsigned(window(j)), SUM_W);
+    end loop;
+    ref_sum <= sum;
+  end process;
+  
   process(clk)
   begin
     if rising_edge(clk) then
